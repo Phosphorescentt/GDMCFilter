de5a47632b140711ff3bef1a305bbcb4dd824d47
Phosphorescentt <joshua@mankelow.org>
2020-03-22 17:28:28 +0000

    Changed heightmap function to actually work

--- settlement_filter.py
+++ settlement_filter.py
@@ -1,29 +1,58 @@
 import time
-from math import sqrt, tan, sin, cos, pi, ceil, floor, acos, atan, asin, degrees, radians, log, atan2, acos, asin
-from random import *
-from numpy import *
-from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
-from mcplatform import *
-
+import math
+import random
+import numpy as np
+import pymclevel as pmcl
+#  from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
+import mcplatform
+#  from mcplatform import *
 import utilityFunctions as utilityFunctions
 
+from pprint import pprint
+
 inputs = (
-	("Mank's test", "label"),
-	("Material", alphaMaterials.Cobblestone),
-        ("Creator: Josh Mankelow", "label"),
-	)
+         ("Mank's test", "label"),
+         ("Material", pmcl.alphaMaterials.Cobblestone),
+         ("Creator: Josh Mankelow", "label"),
+         )
+
+
+class Helper:
+    # Likely to be very inefficient if used across a big box.
+    # TODO Optimise
+    def generate_terrain_heightmap(self, level, box):
+        range_x = box.maxx - box.minx
+        range_y = box.maxy - box.miny
+        range_z = box.maxz - box.minz
+        heightmap = np.zeros((range_x, range_z))
+        for x in range(range_x):
+            adj_x = x + box.minx
+            for z in range(range_z):
+                adj_z = box.minz + z
+                for y in range(range_y, 0, -1):
+                    adj_y = box.miny + y
+                    current_block = level.blockAt(adj_x, adj_y, adj_z)
+                    if current_block != 0:
+                        break
+                    #  while current_block is not pmcl.alphaMaterials.air.ID:
+
+                heightmap[x, z] = adj_y
+
+        return heightmap
 
 
 # 0: Parameters
-# 1: the level (aka the minecraft world).
+# 1: the level (the minecraft world).
 # 2: the selected box from mcedit.
 # 3: User defined inputs from mcedit
 def perform(level, box, options):
+    helper = Helper()
     print("Performing!")
 
-    for x in range(box.minx, box.maxx):
-        for y in range(box.miny, box.maxy):
-            for z in range(box.minz, box.maxz):
-                utilityFunctions.setBlock(level, (options["Material"].ID, 0), x, y, z)
+    #  block = level.blockAt(box.minx, box.miny, box.minz)
+    #  print(block)
+    #  print(pmcl.alphaMaterials.Cobblestone.ID)
+    heightmap = helper.generate_terrain_heightmap(level, box)
+    pprint(heightmap)
 
     print("Ending perform!")
